087d7a3cebcd119ba7fb811eae1a06d28d68e5da
Stephan <stephan@spor.tel>
2020-03-12 21:18:25 +0100

    Changed languages to nl-fr-en only, added nltk installation

--- setup.sh
+++ setup.sh
@@ -9,8 +9,8 @@ DATADIR='/data/'"${USER}"'/EuroNMT'
 # Prepare /data directories
 mkdir "${DATADIR}"
 
-Download and unpack parallel corpora
-for CORPLANG in fr nl fi el; do
+# Download and unpack parallel corpora
+for CORPLANG in fr nl; do
   wget http://www.statmt.org/europarl/v7/"${CORPLANG}"-en.tgz
   mkdir "${CORPLANG}"-en
   tar -xzf "${CORPLANG}"-en.tgz -C "${CORPLANG}"-en
@@ -26,7 +26,7 @@ source "${DATADIR}"/env/bin/activate
 pip install --upgrade pip==20.0.2
 
 # Install required packages (inside virtual environment)
-pip install spacy==2.2.3 spacy-stanfordnlp==0.1.3 pandas==1.0.1 spacy_conll==1.2.0
+pip install spacy==2.2.3 spacy-stanfordnlp==0.1.3 pandas==1.0.1 spacy_conll==1.2.0 nltk==3.4.5
 
 # Download pretrained statistical model for English
 python3 -m spacy download en_core_web_lg-2.2.5
@@ -34,3 +34,4 @@ python3 -m spacy download en_core_web_lg-2.2.5
 # https://stanfordnlp.github.io/stanfordnlp/models.html#human-languages-supported-by-stanfordnlp
 # https://stanfordnlp.github.io/stanfordnlp/performance.html#system-performance-in-conll-2018-shared-task
 python3 -c "import stanfordnlp; stanfordnlp.download('en_ewt', force=True, resource_dir='${DATADIR}')"
+python3 -c "import nltk; nltk.download('punkt')"
