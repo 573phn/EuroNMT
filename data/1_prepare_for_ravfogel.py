9342dcbfd396ab58ecb918b170441e90bf2584a3
Stephan <stephan@spor.tel>
2020-03-30 13:14:01 +0200

    Added intermittent saving of progress while doing CoNLL conversion

--- data/1_prepare_for_ravfogel.py
+++ data/1_prepare_for_ravfogel.py
@@ -2,15 +2,25 @@
 import sys
 import spacy
 import pandas as pd
-#import stanfordnlp
+import glob
 import os
 from shutil import copyfile
-from nltk.tokenize import word_tokenize
 from spacy_conll import ConllFormatter
-#from spacy_stanfordnlp import StanfordNLPLanguage
 
 import stanza
 from spacy_stanza import StanzaLanguage
+
+# Load Stanza (formerly StanfordNLP) and Spacy)
+snlp = stanza.Pipeline(lang='en', processors={'ner': 'ontonotes'},
+                       package='ewt', tokenize_pretokenized=True)
+nlp = StanzaLanguage(snlp)
+# nlp.to_disk("/data/s3212599/EuroNMT")
+conllformatter = ConllFormatter(nlp)
+nlp.add_pipe(conllformatter, last=True)
+sp_en = spacy.load('en_core_web_lg')
+sp_fr = spacy.load('fr_core_news_md')
+sp_nl = spacy.load('nl_core_news_sm')
+chunk_size = 50000
 
 
 def clean(lang):
@@ -93,26 +103,39 @@ def save_to_file(df, output='lang'):
                          f"'{output}'.")
 
 
-def make_conll(df):
-    snlp = stanfordnlp.Pipeline(lang='en', tokenize_pretokenized=True)
-    nlp = StanfordNLPLanguage(snlp)
-    nlp.to_disk("/data/s3212599/EuroNMT")
-    conllformatter = ConllFormatter(nlp)
-    nlp.add_pipe(conllformatter, last=True)
+def do_something_with(row):
+    return nlp(" ".join([w.text for w in sp_en(row, disable=['tagger', 'parser', 'ner'])]))._.conll_str
 
-    for col in [col for col in df.columns if col != 'en']:
-        
-        if not os.path.exists(f'en-{col}'):
-            os.makedirs(f'en-{col}')
-        
-        with open(f'en-{col}/en.conll', 'w') as sentsout:
-            #sentsout.write('\n'.join([nlp(' '.join(word_tokenize(row)))._.conll_str for row in df['en']]))
-            for row in df['en']:
-                print(row)
-                row = " ".join(word_tokenize(row))
-                doc = nlp(row)
-                sentsout.write(f'{doc._.conll_str}\n')
-                print(doc._.conll_str)
+
+def chunks(lst, n):
+    """ Yield successive n-sized chunks from lst.
+        https://stackoverflow.com/a/312464 """
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
+def make_files(df, resume_num=0):
+    print('range to process', range(resume_num, len(df.index)))
+    for chunk in chunks(range(resume_num, len(df.index)), chunk_size):
+        print('processing', chunk)
+        # Process each DataFrame row in chunk and save result to chunk_list
+        chunk_list = []
+        for row in chunk:
+            chunk_list.append(do_something_with(df.at[row, 'en']))
+        # Convert chunk_list to DataFrame
+        chunk_df = pd.DataFrame(chunk_list)
+        # Start chunk DataFrame index from first value in range
+        chunk_df.index += chunk[0]
+        # Save chunk to file
+        chunk_df.to_hdf(f'chunks/{chunk[-1]}.h5', key='chunk_df')
+
+
+def tokenize_lowercase_french(sent):
+    return " ".join([w.text for w in sp_fr(sent, disable=['tagger', 'parser', 'ner'])]).lower()
+
+
+def tokenize_lowercase_dutch(sent):
+    return " ".join([w.text for w in sp_nl(sent, disable=['tagger', 'parser', 'ner'])]).lower()
     
     
 def main():
@@ -157,66 +180,73 @@ def main():
                 print('DataFrame after removing duplicates:')
                 print(merged_dfs.drop_duplicates(subset='en'))
 
+            # Sorting to make sure the rows are always in the same order
+            # Reorder DataFrame columns
+            merged_dfs.sort_index(axis=1, inplace=True)
+            # Sort DataFrame
+            merged_dfs.sort_values(by=sorted(['en'] + [col for col in merged_dfs.columns if col != 'en']), inplace=True)
+            # Reset DataFrame index
+            merged_dfs.index = range(len(merged_dfs.index))
 
-            # Add 'en_conll' column
-            snlp = stanza.Pipeline(lang='en', processors={'ner': 'ontonotes'}, package='ewt', tokenize_pretokenized=True)
-            nlp = StanzaLanguage(snlp)
-            #nlp.to_disk("/data/s3212599/EuroNMT")
-            conllformatter = ConllFormatter(nlp)
-            nlp.add_pipe(conllformatter, last=True)
-            sp_en = spacy.load('en_core_web_lg')
-            
-            #merged_dfs['en_conll'] = nlp(" ".join([w.text for w in sp_en(merged_dfs['en'].str.rstrip(), disable=['tagger', 'parser', 'ner'])]))._.conll_str
-            conll = []
-            for row in merged_dfs['en']:
-                conll.append(nlp(" ".join([w.text for w in sp_en(row, disable=['tagger', 'parser', 'ner'])]))._.conll_str)
-            merged_dfs['en_conll'] = conll
-            
+            # TODO: Add saving code
+            # Check which chunks have already been done and resume from last one
+            existing_files = glob.glob('/home/s3212599/EuroNMT/data/chunks/*.h5')
+            existing_files = [int(os.path.splitext(os.path.basename(ef))[0])
+                              for ef in existing_files]
+            print('existing files', existing_files)
+
+            print('df chunks', list(chunks(range(len(merged_dfs.index)), chunk_size)))
+            chunk_ends = [chunk[-1] for chunk in
+                          list(chunks(range(len(merged_dfs.index)), chunk_size))]
+            print('chunk_ends', chunk_ends)
+
+            if existing_files:
+                if max(existing_files) == max(chunk_ends):
+                    # all files already made, no action required
+                    print('all files have already been made')
+
+                else:
+                    # resume
+                    print('resume from ', max(existing_files) + 1)
+                    resume_num = max(existing_files) + 1
+                    make_files(merged_dfs, resume_num)
+            else:
+                # start
+                print('start')
+                make_files(merged_dfs)
+
+            # Combine all chunks
+            all_files = sorted(glob.glob('/home/s3212599/EuroNMT/data/chunks/*.h5'))
+            combined_chunks = pd.concat([pd.read_hdf(f) for f in all_files])
+            combined_chunks.to_hdf('combined_chunks.h5', key='combined_chunks')
+
+            print('saved DataFrame:')
+            combined_save = pd.read_hdf('combined_chunks.h5')
+            print(combined_save)
+
+            # Add column from saved DataFrame to original DataFrame
+            merged_dfs['en_conll'] = combined_save[0]
+            print(merged_dfs)
+
             print('CoNLL column:')
             print(merged_dfs['en_conll'])
             print('--------')
-            
-            # Save 'en_conll' column to file
+
             # Prepare folders
-            foreign_langs = [col for col in merged_dfs.columns if col != 'en' and col != 'en_conll']
+            foreign_langs = [col for col in merged_dfs.columns if
+                             col != 'en' and col != 'en_conll']
             for col in foreign_langs:
                 if not os.path.exists(f'en-{col}'):
                     os.makedirs(f'en-{col}')
-            
+
             # Save file to first language
             with open(f'en-{foreign_langs[0]}/en.conll', 'w') as sentsout:
-                sentsout.write(df['en_conll'].to_csv(header=False, index=False))
+                sentsout.write(merged_dfs['en_conll'].to_csv(header=False, index=False))
 
             # Copy created file to folders of other languages
             if len(foreign_langs) > 1:
                 for lang in foreign_langs[1:]:
                     copyfile(f'en-{foreign_langs[0]}/en.conll', f'en-{lang}/en.conll')
-            
-            """
-            #make_conll(merged_dfs)
-            snlp = stanfordnlp.Pipeline(lang='en', tokenize_pretokenized=True)
-            nlp = StanfordNLPLanguage(snlp)
-            nlp.to_disk("/data/s3212599/EuroNMT")
-            conllformatter = ConllFormatter(nlp)
-            nlp.add_pipe(conllformatter, last=True)
-            sp_en = spacy.load('en_core_web_lg')
-
-            foreign_langs = [col for col in merged_dfs.columns if col != 'en']
-            for col in foreign_langs:
-                if not os.path.exists(f'en-{col}'):
-                    os.makedirs(f'en-{col}')            
-
-            with open(f'en-{foreign_langs[0]}/en.conll', 'w') as sentsout:
-                #sentsout.write('\n'.join([nlp(' '.join(word_tokenize(row)))._.conll_str for row in df['en']]))
-                for row in merged_dfs['en']:
-                    row = " ".join(word_tokenize(row))
-                    doc = nlp(row)
-                    sentsout.write(f'{doc._.conll_str}\n')
-                    #print(doc._.conll_str)
-                            
-            if len(foreign_langs) > 1:
-                for lang in foreign_langs[1:]:
-                    copyfile(f'en-{foreign_langs[0]}/en.conll', f'en-{lang}/en.conll')"""
 
             # Save output to file(s), output argument should be 'lang' or 'tsv'
             save_to_file(df=merged_dfs, output='lang')
