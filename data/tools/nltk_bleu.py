fa5fe42fe5f28c108875ce44884a6d91ec240808
573phn <stephan@spor.tel>
2020-07-05 21:46:41 +0200

    Simplified bleu function.

--- data/tools/nltk_bleu.py
+++ data/tools/nltk_bleu.py
@@ -5,27 +5,19 @@
 sent bleu using nltk
 Modified version of: https://github.com/cshanbo/Smooth_BLEU
 '''
-from nltk.translate.bleu_score import SmoothingFunction, sentence_bleu, corpus_bleu
+from nltk.translate.bleu_score import SmoothingFunction, corpus_bleu
 import nltk
 import argparse
 
-def bleu_calculation(reference='', translation='',
-                     sentence_level=False, output='', weights='0.25 0.25 0.25 0.25'):
+def bleu_calculation(reference='', translation='', weights='0.25 0.25 0.25 0.25'):
     weight = [float(v) for v in weights.split()]
     with open(translation, 'r') as trans, open(reference, 'r') as ref:
-        ref_list = [[rl.strip().split(' ')] for rl in ref]
-        tran_list = [tl.strip().split(' ') for tl in trans]
-        if sentence_level:
-            if output is None:
-                print(sentence_bleu([ref_list], tran_list, weight))
-            else:
-                with open(output, 'w') as outputfile:
-                    outputfile.write('%s\n' % sentence_bleu([ref_list], tran_list, weight))
-        else:
-            # https://www.nltk.org/api/nltk.translate.html#module-nltk.translate.bleu_score
-            chencherry = SmoothingFunction()
-            print(corpus_bleu(ref_list, tran_list, weight, smoothing_function=chencherry.method3))
-        return
+        ref_list = [[rl.strip().split()] for rl in ref]
+        tran_list = [tl.strip().split() for tl in trans]
+        # https://www.nltk.org/api/nltk.translate.html#module-nltk.translate.bleu_score
+        chencherry = SmoothingFunction()
+        print(corpus_bleu(ref_list, tran_list, weight, smoothing_function=chencherry.method3))
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Arguments for calculating BLEU')
@@ -33,11 +25,6 @@ if __name__ == '__main__':
                          help="reference file")
     parser.add_argument('-t', '--translation', type=str, required=True, 
                          help="translation file")
-    parser.add_argument('-o', '--output', type=str, default=None, required=False, 
-                        help="output BLEU score to this file in segment level scenario \
-                             (default: %(default)s)")
-    parser.add_argument('-sl', '--sentence-level', action='store_true',
-                         help="print segment level BLEU score (default: %(default)s)")
     parser.add_argument('-w', '--weights', type=str, required=False, 
                          default='0.25 0.25 0.25 0.25',
                          help="weights for n-grams (default: %(default)s)")
@@ -45,6 +32,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     bleu_calculation(reference=args.reference,
                      translation=args.translation,
-                     sentence_level=args.sentence_level,
-                     output=args.output, 
                      weights=args.weights)
