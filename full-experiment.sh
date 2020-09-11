3bf7f54185b53ea5cc5123b09158dcca1bc0fd48
573phn <stephan@spor.tel>
2020-09-11 17:35:09 +0200

    Added missing word order (en_shuffle)

--- full-experiment.sh
+++ full-experiment.sh
@@ -9,7 +9,7 @@
 echo "${@}"
 
 # Set variables
-WOS='en en_random en_sov en_svo en_vos30rest14 en_vos60rest8 en_vos en_vso30rest14 en_vso60rest8 en_vso en_randomna-aseed5 en_randomna-dseed5 en_randomnoneseed10'
+WOS='en en_random en_sov en_vos en_vos30rest14 en_vos60rest8 en_vso en_vso30rest14 en_vso60rest8 en_svo en_randomna-aseed5 en_randomna-dseed5 en_randomnoneseed10 en_shuffle'
 
 for WO in ${WOS}; do
   sbatch preprocess.sh fr "${WO}"
